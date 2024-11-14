1167bc756edd4af37464b666251b8374b8caea16
Garsman123 <garsman@gmail.com>
2024-11-14 22:02:56 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-d15d922b-cf04-4224-ae08-000bbb4550a7.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-c67d163b-26bc-4c11-917f-8c18dd44b82c.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
